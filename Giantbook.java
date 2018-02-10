11d341da3ea68677a462ea1c8d853f8533135764
Hlaursen <Hlaursen86@gmail.com>
2018-02-10 22:57:14 +0100

    Test

--- Giantbook.java
+++ Giantbook.java
@@ -1,66 +1,64 @@
-// Import MyUnionFind.java
 import edu.princeton.cs.algs4.*;
 import one.MyUnionFind;
 
 
-public class Giantbook
+public class statGiantbook
 {
     public static void main(String[] args)
     {
-      // Generate u and v uniformly at random (how to specify how many to choose from?)
-      /*Read from StdIn a number n, then n pairs of numbers u and v. An event happens
-      at time i if it happens after i edges have been read*/
       int n = StdIn.readInt(); //Size of data structure
-      int i = 0; //Number of edges that have been read
-      int i_isolated = -1; // Number of edges read when no more isolated nodes
-      int i_giant = -1; // Number of edges read when graph has a giant component
-      int i_connected = -1; // Number of edges read when the graph is connected
+      int T = StdIn.readInt(); //Number of times to repeat experiment
+      int[][] topResults = new int[T][3];
 
-      MyUnionFind uf = new MyUnionFind(n);
-
-      while (!StdIn.isEmpty())
+      //Run experiment T times
+      for (int t=0; t<T; t++)
       {
-        int p = StdIn.readInt();
-        int q = StdIn.readInt();
-        i++; // Increment number of edges
-        if (uf.connected(p, q)) continue;
-        uf.union(p, q);
+        int i_isolated = -1; // Number of edges read when no more isolated nodes
+        int i_giant = -1; // Number of edges read when graph has a giant component
+        int i_connected = -1; // Number of edges read when the graph is connected
+        int results[] = new int[3];
+        MyUnionFind uf = new MyUnionFind(n);
 
-        // Giant component emerges
-        if (i_giant == -1 && uf.maxComponentSize() >= (n/2)) {
-          i_giant = i;
+        //Generate UF data structure and add connections until the connected component emerges
+        int i = 0; //Number of edges that have been read
+        while (true)
+        {
+          int p = StdRandom.uniform(n);
+          int q = StdRandom.uniform(n);
+          i++; // Increment number of edges
+          if (uf.connected(p, q)) continue;
+          uf.union(p, q);
+
+          // Giant component emerges
+          if (i_giant == -1 && uf.maxComponentSize() >= (n/2)) {
+            i_giant = i;
+          }
+
+          // Isolated
+          if (i_isolated == -1 && uf.isolated() == 0) {
+            i_isolated = i;
+          }
+
+          // Connected
+          if (i_connected == -1 && uf.count() == 1) {
+            i_connected = i;
+            results[0] = i_isolated;
+            results[1] = i_giant;
+            results[2] = i_connected;
+            break;
+          }
         }
-
-        // Isolated
-        if (i_isolated == -1 && uf.isolated() == 0) {
-          i_isolated = i;
-        }
-
-        // Connected
-        if (i_connected == -1 && uf.count() == 1) {
-          i_connected = i;
-          break;
-
-        }
+        topResults[t] = results;
       }
 
-      /* The output of your program is a single line with four numbers:
-      n, the number of vertices (that's how we can use it for a plot)
-      the first time the graph has no more isolated vertices
-      the first time that the graph has a giant component
-      the first time that the graph is connected. */
-      StdOut.println("Number of components: "+ n + " \nNumber of random connections before connected: " + i_connected + " \nNumber of components: " + uf.count());
-      StdOut.println("Max component size: " + uf.maxComponentSize());
-      StdOut.println("Number of rand connections before giant component: " + i_giant);
-      StdOut.println("Number of rand connections before non-isolated: " + i_isolated);
-
+      // Print results
+      StdOut.println("\nUF size: " + n);
+      for (int t=0; t<T; t++)
+      {
+        StdOut.println("Experiment number: " + (t+1));
+        StdOut.println("Isolated: " + topResults[t][0]);
+        StdOut.println("Giant : " + topResults[t][1]);
+        StdOut.println("Connected: " + topResults[t][2]);
+      }
     }
-
-
-
-
-
-
-
-
 }
