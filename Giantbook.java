f97122d451a20b104aad5bc8752881007c8f6704
Hlaursen <Hlaursen86@gmail.com>
2018-02-10 23:53:47 +0100

    Giantbook updated

--- Giantbook.java
+++ Giantbook.java
@@ -1,16 +1,15 @@
 import edu.princeton.cs.algs4.*;
 import one.MyUnionFind;
 
-
 public class Giantbook
 {
     public static void main(String[] args)
     {
       StdOut.println("Input size of MyUnionFind datastructure, n:");
-      int n = StdIn.readInt(); //Size of data structure
+      int n = StdIn.readInt();                      //Size of data structure
       StdOut.println("Input number of times to repeat, T:");
-      int T = StdIn.readInt(); //Number of times to repeat experiment
-      int[][] topResults = new int[T][3];
+      int T = StdIn.readInt();                      //Number of times to repeat experiment
+      int[][] collectedResults = new int[3][T];     // Collects counts for each experiment
 
       //Run experiment T times
       for (int t=0; t<T; t++)
@@ -22,12 +21,12 @@ public class Giantbook
         MyUnionFind uf = new MyUnionFind(n);
 
         //Generate UF data structure and add connections until the connected component emerges
-        int i = 0; //Number of edges that have been read/
+        int i = 0;      //Number of edges that have been read/
         while (true)
         {
           int p = StdRandom.uniform(n);
           int q = StdRandom.uniform(n);
-          i++; // Increment number of edges
+          i++;        // Increment number of edges
           if (uf.connected(p, q)) continue;
           uf.union(p, q);
 
@@ -36,31 +35,46 @@ public class Giantbook
             i_giant = i;
           }
 
-          // Isolated
-          if (i_isolated == -1 && uf.isolated() == 0) {
+          // No more isolated components exist
+          if (i_isolated == -1 && uf.isolatedComponents() == 0) {
             i_isolated = i;
           }
 
-          // Connected
+          // Connected component emerges
           if (i_connected == -1 && uf.count() == 1) {
             i_connected = i;
-            results[0] = i_isolated;
-            results[1] = i_giant;
-            results[2] = i_connected;
+            collectedResults[0][t] = i_isolated;
+            collectedResults[1][t] = i_giant;
+            collectedResults[2][t] = i_connected;
             break;
           }
         }
-        topResults[t] = results;
       }
+
+      // Statistical calculations
+      double isolatedMean = StdStats.mean(collectedResults[0]);
+      double isolatedStdDev = StdStats.stddev(collectedResults[0]);
+      double giantMean = StdStats.mean(collectedResults[1]);
+      double giantStdDev = StdStats.stddev(collectedResults[1]);
+      double connectedMean = StdStats.mean(collectedResults[2]);
+      double connectedStdDev = StdStats.stddev(collectedResults[2]);
 
       // Print results
       StdOut.println("\nUF size: " + n);
       for (int t=0; t<T; t++)
       {
-        StdOut.println("Experiment number: " + (t+1));
-        StdOut.println("Isolated: " + topResults[t][0]);
-        StdOut.println("Giant : " + topResults[t][1]);
-        StdOut.println("Connected: " + topResults[t][2]);
+        StdOut.println("EXPERIMENT NUMBER: " + (t+1));
+        StdOut.println("No more isolated components emerge after random connection number: " + collectedResults[0][t]);
+        StdOut.println("Giant component emerges after random connection number: " + collectedResults[1][t]);
+        StdOut.println("Connected component emerges after random connection number: " + collectedResults[2][t]);
       }
+
+      StdOut.println("\nisolatedMean: " + isolatedMean);
+      StdOut.println("isolatedStdDev: " + isolatedStdDev);
+      StdOut.println("giantMean: " + giantMean);
+      StdOut.println("giantStdDev: " + giantStdDev);
+      StdOut.println("connectedMean: " + connectedMean);
+      StdOut.println("connectedStdDev: " + connectedStdDev);
+
     }
 }
